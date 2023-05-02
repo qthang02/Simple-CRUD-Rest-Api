f2627ffaeadfae33622f2fa20fcb48b489bb7d44
qthang02 <nguyenquocthang909@gmail.com>
2023-05-02 17:31:20 +0700

    added Gin, GORM libs, and config .env

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"os"
+	"time"
+)
+
+type Product struct {
+	Id          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Status      string     `json:"status"`
+	Price       float64    `json:"price"`
+	CreatedAt   *time.Time `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	dsn := os.Getenv("DB_CONN")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(db)
+
+	r := gin.Default()
+
+	v1 := r.Group("/v1")
+	{
+		products := v1.Group("/products")
+		{
+			products.GET("")
+			products.GET("/:id")
+			products.POST("")
+			products.PATCH("/:id")
+			products.DELETE("/:id")
+		}
+	}
+
+	if err := r.Run(); err != nil {
+		panic(err)
+	}
 }
